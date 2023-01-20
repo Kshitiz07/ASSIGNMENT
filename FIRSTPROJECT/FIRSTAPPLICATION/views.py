181cec4b74d7fad9fbcd70341bd7a55d0db2fcbc
Kshitiz Bhattarai <xhitiz1@gmail.com>
2023-01-20 03:05:33 -0500

    Constraints2
    --Constraints2 added features

--- FIRSTPROJECT/FIRSTAPPLICATION/views.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
+
+# def home(request):
+#     template = loader.get_template('FIRSTAPPLICATION/base.html')
+#     return HttpResponse(template.render())
 
 def home(request):
-    template = loader.get_template('FIRSTAPPLICATION/base.html')
-    return HttpResponse(template.render())
+    return render(request, 'FIRSTAPPLICATION/home.html')
